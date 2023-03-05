16504ecb96c846ac85accbb46eed53a7865ebe0c
mutish <mutindastacey@gmail.com>
2023-03-05 16:57:21 +0300

    updated

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Catalogue(models.Model):
     product_name = models.CharField(max_length=30)
-    product_image = models.ImageField()
+    product_image = models.ImageField(width_field=50, height_field=50)
     product_price = models.CharField(max_length=10)
     class Meta:
         db_table = "catalogue"
